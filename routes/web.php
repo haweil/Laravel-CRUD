682b0ac2544ccc219c9d57fa308b6061b933a9a3
haweil <92003311+haweil@users.noreply.github.com>
2024-05-07 09:08:52 +0300

    'Update Route File'

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products\create');
 });
 
 //Route::get('/products/create',[ProductController::class,'create'])->name('products.create');
